147d6c02c18617f002157a6ea6e5b2442aea0600
machleslie <machariales@gmail.com>
2023-09-20 12:54:49 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+function callback(){
+    console.log(`Hello`);
+}
+function receivesAFunction(callback){
+    callback();
+}
+function afun (){
+return function named(){}
+}
+function returnsANamedFunction(){
+    return(afun);
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {
+      
+    };
+   }
+   
+  
